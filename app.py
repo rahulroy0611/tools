dcdeb9173f32228b95090671cea9a3f298be95e5
Rahul Roy <rahulroy0611@gmail.com>
2024-09-17 02:47:36 +0530

    fix an error

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def process_page(pdf_path, page, drop_columns):
 
 
 def process_pdf(file_path):
-
+   
     df = tabula.read_pdf(file_path, pages=1, multiple_tables=True)[1]
     df = pd.concat([pd.DataFrame([df.columns], columns=df.columns), df], ignore_index=True)
 
@@ -30,9 +30,11 @@ def process_pdf(file_path):
 
     data = pd.concat([df_1, df_2, df_3, df_4], axis=0)
 
+  
     data.columns = df.iloc[0].values
     data.replace(r'^Unnamed: \d+$', np.nan, regex=True, inplace=True)
 
+ 
     def Layer(value):
         match = re.search(r'Layer : (\d+)', value)
         return int(match.group(1)) if match else None
@@ -40,21 +42,10 @@ def process_pdf(file_path):
     def Money(value):
         if pd.isna(value):
             return None
+        match = re.search(r'Disputed Amount: (\d+)', str(value))
+        return int(match.group(1)) if match else None
 
-        # Remove non-numeric characters and leading/trailing spaces
-        value = re.sub(r'[^\d]', '', str(value)).strip()
-
-        # Check if the value is empty after cleaning
-        if not value:
-            return None
-
-        try:
-            return int(value)
-        except ValueError:
-            # Handle cases where the value still cannot be converted to int
-            # You might want to log this or return a default value
-            return None
-
+ 
     data['Amount'] = data['Transaction Details'].apply(Money)
     data['Layer'] = data['Account\rNo./ (Wallet\r/PG/PA) Id\rTransaction\rId / UTR\rNumber'].apply(Layer)
 
@@ -75,8 +66,9 @@ if uploaded_file is not None:
 
     if st.button("Convert to Excel"):
         with st.spinner('Processing...'):
-
+           
             processed_data = process_pdf("temp_pdf.pdf")
+
 
             output_excel = "converted_data.xlsx"
             processed_data.to_excel(output_excel, index=False)
@@ -89,8 +81,7 @@ if uploaded_file is not None:
                     file_name="converted_data.xlsx",
                     mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
                 )
-
-            # Existing Charts
+            
             st.subheader("Distribution of Disputed Amounts")
             fig, ax = plt.subplots()
             ax.hist(processed_data['Amount'], bins='auto')
@@ -98,11 +89,10 @@ if uploaded_file is not None:
             ax.set_ylabel('Frequency')
             st.pyplot(fig)
 
+            # Chart 2: Average disputed amount per layer
             st.subheader("Average Disputed Amount by Layer")
             avg_amount_per_layer = processed_data.groupby('Layer')['Amount'].mean()
             st.bar_chart(avg_amount_per_layer)
-
-            
 
 
     if os.path.exists("temp_pdf.pdf"):
